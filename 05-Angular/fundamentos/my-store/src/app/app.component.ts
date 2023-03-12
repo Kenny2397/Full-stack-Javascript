57f18bb933419534d405488033794faa39b723ce
khaeldev <kenny.luque.t@uni.pe>
2023-03-12 17:43:25 -0500

    Event binding

--- 05-Angular/fundamentos/my-store/src/app/app.component.ts
+++ 05-Angular/fundamentos/my-store/src/app/app.component.ts
@@ -7,8 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   name = 'kenny';
-  age = '24'
+  age: number = 24
   img = 'https://images.unsplash.com/photo-1677714548607-61fa754cdd2d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY3ODY1OTI5Mw&ixlib=rb-4.0.3&q=80&w=1080'
   btnDisabled = true
   textValue = 'textooooo'
+  person = {
+    name: 'kenny',
+    age: 24,
+    img: 'https://images.unsplash.com/photo-1677714548607-61fa754cdd2d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY3ODY1OTI5Mw&ixlib=rb-4.0.3&q=80&w=1080'
+  }
+
+  toggleButton(){
+    this.btnDisabled = !this.btnDisabled
+  }
+
+  incrementAge(){
+    this.person.age += 1
+  }
 }
