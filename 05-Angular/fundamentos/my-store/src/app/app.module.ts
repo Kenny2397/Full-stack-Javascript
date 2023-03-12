743bd60268bcac6b2f0dc82df3efdf977c2d12d9
khaeldev <kenny.luque.t@uni.pe>
2023-03-12 18:30:10 -0500

    ngModel | angular/forms

--- 05-Angular/fundamentos/my-store/src/app/app.module.ts
+++ 05-Angular/fundamentos/my-store/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +12,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
