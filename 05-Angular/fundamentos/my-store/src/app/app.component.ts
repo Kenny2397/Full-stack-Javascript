1b2243ed88dd6de80cdc10b7dd7ac2815b6c07f9
khaeldev <kenny.luque.t@uni.pe>
2023-03-12 17:57:08 -0500

    event binding 2

--- 05-Angular/fundamentos/my-store/src/app/app.component.ts
+++ 05-Angular/fundamentos/my-store/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   name = 'kenny';
@@ -23,5 +23,16 @@ export class AppComponent {
 
   incrementAge(){
     this.person.age += 1
+  }
+
+  onScroll(event: Event){
+    const element = event.target as HTMLElement
+    console.log(element.scrollTop)
+
+  }
+
+  changeName(event: Event){
+    const element = event.target as HTMLInputElement
+    this.person.name =  element.value
   }
 }
