6651d8c674dd0c6d91a72ca72a1303a9f69de9ea
khaeldev <kenny.luque.t@uni.pe>
2023-03-25 10:29:31 -0500

    migration order-product

---
+++ 02-backend-express/my-store/db/migrations/20230325152619-order-product.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const { ORDER_PRODUCT_TABLE, OrderProductSchema } = require('../models/order-product.model')
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface) {
+
+    await queryInterface.createTable(ORDER_PRODUCT_TABLE, OrderProductSchema);
+
+  },
+
+  async down (queryInterface) {
+
+    await queryInterface.dropTable(ORDER_PRODUCT_TABLE);
+
+  }
+};
